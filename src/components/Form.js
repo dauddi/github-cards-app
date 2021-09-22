bb728d6679e0921a0c7e7314589421258813e247
Davis Okioma <kuntaytc@gmail.com>
2021-09-22 15:14:10 +0300

    updated uncontrolled input in Form component to controlled

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from 'axios';
 import "./css/form.css"
 import Clear from './Clear';
 
 function Form(props){
-	let newUserName;
+	const [userNameInput, setUserNameInput] = useState('')
 	const onChangeHandler = (e) => {
-		newUserName = e.target.value
+		setUserNameInput(e.target.value)
 	}
 
 	const onSubmitHandler = async (e) => {
 		e.preventDefault();
 		
-		const response = await axios.get(`https://api.github.com/users/${newUserName}`);
+		const response = await axios.get(`https://api.github.com/users/${userNameInput}`);
 		const data = await response.data
 
 		props.userDataHandler([data, ...props.currentState]);
-		return ''
+		setUserNameInput('')
 	}
 
 
 	return(
 		<form onSubmit={onSubmitHandler} className="form">
-			<input onChange={onChangeHandler} type="text" placeholder="github username" required />
+			<input onChange={onChangeHandler} value={userNameInput} type="text" placeholder="github username" required />
 			<button>Add User</button>
 			<Clear userDataHandler={props.userDataHandler} />
 		</form>
